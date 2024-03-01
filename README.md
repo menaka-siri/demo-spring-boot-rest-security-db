c685004e01f09d191ce68701c6ce43b95b55a234
Menaka S <menaka442@gmail.com>
2024-03-01 17:53:23 +0530

    save user credentials in db

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# demo-spring-boot-rest-security
-demo-spring-boot-rest-security
+# demo-spring-boot-rest-security-db
+demo-spring-boot-rest-security demo where user credentials are stored in the db.
--- src/main/java/com/zealtrack/employeecrud/security/DemoSecurityConfig.java
+++ src/main/java/com/zealtrack/employeecrud/security/DemoSecurityConfig.java
@@ -5,14 +5,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    @Bean
+    /*@Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails john = User.builder()
                 .username("john")
@@ -33,6 +34,12 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }*/
+
+    //add support for JDBC ... no more hardcoded users
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
